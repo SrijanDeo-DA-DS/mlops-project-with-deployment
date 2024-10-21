ee48f1b608def1c9e8ccf65dbbfff920ddd9b990
SrijanDeo-DA-DS <sd759@njit.edu>
2024-10-21 16:44:49 -0500

    add project structure

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='This project will predict delivery time of food items from a restaurant using Regression ML algorithms',
+    author='SrijanDeo',
+    license='',
+)
